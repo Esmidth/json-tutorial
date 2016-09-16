c3282ea8255245e5853cd1d5f05627fbad9c00ab
Qingyi Chang <esmidth@163.com>
2016-09-17 02:46:25 +0800

    Finally fix LEPT_PARSE_ROOT_NOT_SINGULAR

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -5,43 +5,77 @@
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
 
 typedef struct {
-    const char* json;
-}lept_context;
+	const char* json;
+} lept_context;
 
 static void lept_parse_whitespace(lept_context* c) {
-    const char *p = c->json;
-    while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
-        p++;
-    c->json = p;
+	const char* p = c->json;
+	while (*p == ' ' || *p == '\t' || *p == '\n' || *p == 'r') {
+		p++;
+	}
+	c->json = p;
 }
 
 static int lept_parse_null(lept_context* c, lept_value* v) {
-    EXPECT(c, 'n');
-    if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 3;
-    v->type = LEPT_NULL;
-    return LEPT_PARSE_OK;
+	EXPECT(c, 'n');
+	if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l') {
+		return LEPT_PARSE_INVALID_VALUE;
+	}
+	c->json += 3;
+	v->type = LEPT_NULL;
+	return 0;
 }
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
-    switch (*c->json) {
-        case 'n':  return lept_parse_null(c, v);
-        case '\0': return LEPT_PARSE_EXPECT_VALUE;
-        default:   return LEPT_PARSE_INVALID_VALUE;
-    }
+	switch (*c->json) {
+	case 'n':
+		return lept_parse_null(c, v);
+	case '\0':
+		return LEPT_PARSE_EXPECT_VALUE;
+	default:
+		return LEPT_PARSE_INVALID_VALUE;
+	}
+}
+
+static int lept_parse_root_not_singular(lept_context* c) {
+	if (c == NULL)
+		return LEPT_PARSE_OK;
+	const char* p = c->json;
+	while (p != NULL && *p != ' ' && *p != '\t' && *p != '\n' && *p != '\r' && *p != '\0') {
+		p++;
+	}
+	if (p != NULL && *p == '\0') {
+		return LEPT_PARSE_OK;
+	}
+	else 
+		return LEPT_PARSE_ROOT_NOT_SINGULAR;
 }
 
 int lept_parse(lept_value* v, const char* json) {
-    lept_context c;
-    assert(v != NULL);
-    c.json = json;
-    v->type = LEPT_NULL;
-    lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+	/*
+	lept_context c;
+	assert(v != NULL);
+	c.json = json;
+	v->type = LEPT_NULL;
+	lept_parse_whitespace(&c);
+	return lept_parse_value(&c, v);
+	*/
+	lept_context c;
+	assert(v != NULL);
+	c.json = json;
+	v->type = LEPT_NULL;
+	lept_parse_whitespace(&c);
+	int lept_i = lept_parse_value(&c, v);
+	int lept_ii = lept_parse_root_not_singular(&c);
+	if (lept_ii == LEPT_PARSE_OK) {
+		return lept_i;
+	}
+	else
+		return LEPT_PARSE_ROOT_NOT_SINGULAR;
+
 }
 
 lept_type lept_get_type(const lept_value* v) {
-    assert(v != NULL);
-    return v->type;
+	assert(v != NULL);
+	return v->type;
 }
--- tutorial01/test.c
+++ tutorial01/test.c
@@ -10,8 +10,9 @@ static int test_pass = 0;
 #define EXPECT_EQ_BASE(equality, expect, actual, format) \
     do {\
         test_count++;\
-        if (equality)\
+        if (equality){\
             test_pass++;\
+		}\
         else {\
             fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual);\
             main_ret = 1;\
@@ -21,10 +22,10 @@ static int test_pass = 0;
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 
 static void test_parse_null() {
-    lept_value v;
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+	lept_value v;
+	v.type = LEPT_TRUE;
+	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
+	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
 static void test_parse_expect_value() {
@@ -58,14 +59,15 @@ static void test_parse_root_not_singular() {
 }
 
 static void test_parse() {
-    test_parse_null();
-    test_parse_expect_value();
-    test_parse_invalid_value();
-    test_parse_root_not_singular();
+	test_parse_null();
+	test_parse_expect_value();
+	test_parse_invalid_value();
+	test_parse_root_not_singular();
 }
 
 int main() {
-    test_parse();
-    printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
-    return main_ret;
+	test_parse();
+	printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
+	system("pause");
+	return main_ret;
 }
