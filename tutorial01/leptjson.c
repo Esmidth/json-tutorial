800afc02ce4a315a6433c3dde9b0f9afd2b235a8
Qingyi Chang <esmidth@163.com>
2016-09-17 15:22:24 +0800

    Fix parse LEPT_TRUE & LEPT_FALSE Complete test_parse_true() & test_parse_false()

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -26,12 +26,34 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
 	return 0;
 }
 
+static int lept_parse_true(lept_context* c, lept_value* v) {
+	EXPECT(c, 't');
+	if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e') {
+		return LEPT_PARSE_INVALID_VALUE;
+	}
+	v->type = LEPT_TRUE;
+	return LEPT_PARSE_OK;
+}
+
+static int lept_parse_false(lept_context* c, lept_value* v) {
+	EXPECT(c, 'f');
+	if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e') {
+		return LEPT_PARSE_INVALID_VALUE;
+	}
+	v->type = LEPT_FALSE;
+	return LEPT_PARSE_OK;
+}
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
 	switch (*c->json) {
 	case 'n':
 		return lept_parse_null(c, v);
 	case '\0':
 		return LEPT_PARSE_EXPECT_VALUE;
+	case 't':
+		return lept_parse_true(c, v);
+	case 'f':
+		return lept_parse_false(c,v);
 	default:
 		return LEPT_PARSE_INVALID_VALUE;
 	}
@@ -47,7 +69,7 @@ static int lept_parse_root_not_singular(lept_context* c) {
 	if (p != NULL && *p == '\0') {
 		return LEPT_PARSE_OK;
 	}
-	else 
+	else
 		return LEPT_PARSE_ROOT_NOT_SINGULAR;
 }
 
--- tutorial01/test.c
+++ tutorial01/test.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdlib.h> // NULL
 #include "leptjson.h"
 
 static int main_ret = 0;
@@ -29,40 +28,57 @@ static void test_parse_null() {
 }
 
 static void test_parse_expect_value() {
-    lept_value v;
+	lept_value v;
 
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, ""));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+	v.type = LEPT_FALSE;
+	EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, ""));
+	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, " "));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+	v.type = LEPT_FALSE;
+	EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, " "));
+	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
 static void test_parse_invalid_value() {
-    lept_value v;
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "nul"));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+	lept_value v;
+	v.type = LEPT_FALSE;
+	EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "nul"));
+	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "?"));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+	v.type = LEPT_FALSE;
+	EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "?"));
+	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
 static void test_parse_root_not_singular() {
-    lept_value v;
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "null x"));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+	lept_value v;
+	v.type = LEPT_FALSE;
+	EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "null x"));
+	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
+
+static void test_parse_true() {
+	lept_value v;
+	v.type = LEPT_TRUE;
+	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "true"));
+	EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
+}
+
+static void test_parse_false() {
+	lept_value v;
+	v.type = LEPT_NULL;
+	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "false"));
+	EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
+}
+
 
 static void test_parse() {
 	test_parse_null();
 	test_parse_expect_value();
 	test_parse_invalid_value();
 	test_parse_root_not_singular();
+	test_parse_true();
+	test_parse_false();
 }
 
 int main() {
